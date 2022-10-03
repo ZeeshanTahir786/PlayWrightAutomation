de0e0e41788bd7910d0dbfff3c091c6a8919945f
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-10-03 18:30:51 +0500

    first commit

---
+++ tests/UIBasicTest.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require("@playwright/test");
+
+test("Browser Context Platwight test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const title = await page.title();
+  console.log(title);
+  await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+});
+
+test("Page Playwright test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test.only("validate sign in", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+  await page.locator("#username").type("rahulshettyacademy");
+  await page.locator("#password").type("learnin");
+
+  await page.locator("#signInBtn").click();
+  console.log(await page.locator("div[style='display: block;']").textContent());
+  await expect(page.locator("div[style='display: block;']")).toContainText(
+    "Incorrect"
+  );
+});
