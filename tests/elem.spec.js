6cb280a644ffaa9d7c46a2a9f6e374e8af1876e2
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-10-20 16:13:49 +0500

    Hide and show input field

---
+++ tests/elem.spec.js
@@ -0,0 +1,18 @@
+const { default: test, expect } = require("@playwright/test");
+
+test.describe("Hide and show", () => {
+  test.use({
+    viewport: {
+      width: 1920,
+      height: 1080,
+    },
+  });
+  test("Input field", async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+  });
+});
