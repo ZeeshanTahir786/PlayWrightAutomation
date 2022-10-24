8840c2f731807cb3612d76ed13a96ae411f7778b
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-10-24 12:53:45 +0500

    Handle iFrames in playwright

--- tests/elem.spec.js
+++ tests/elem.spec.js
@@ -14,7 +14,22 @@ test.describe("Hide and show", () => {
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
-
     await page.locator("#name").type("Hafiz");
+    await page.locator("#confirmbtn").click();
+    await page.on("dialog", (dialog) => dialog.accept());
+    await page.locator("#mousehover").hover();
+  });
+  test.only("Handlee iframes", async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    const framePage = page.frameLocator("#courses-iframe");
+    await framePage
+      .locator(
+        "//a[@class='new-navbar-highlighter'][normalize-space()='All Access plan']"
+      )
+      .click();
+    const text = await framePage.locator("div[class='text'] h2").textContent();
+    console.log(text.split(" ")[1]);
   });
 });
